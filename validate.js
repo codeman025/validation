7811b955102b949d0765cd6239f31b5735429e80
codeman <clong@catalyte.io>
2018-04-26 11:59:51 -0700

    added min and max variables to compare to item.length property

--- validate.js
+++ validate.js
@@ -10,25 +10,36 @@ function validateForm() {
 //each field will be passed into various functions, and if it fails, it will return false with a descriptive error message
 // going to need to break up the forms into form 1 and for 2
 // probably just make 2 separate functions to deal with that.
-
+//TODO:
 function form1(){
     // validates only form 1's things, separate from form 2
 }
 function form2(){
     //validates only the second 5 items
+    if(bananas && weekday && phone && alphaID && painLevel == true)
+    validate this form
+    else {
+        display error messages
+    }
 }
 
 function required_size(field, min, max){
     let item = document.getElementById("field").value // sets item to the value of whatever field i am working with;
-    if(item.length < min)
+    if(item.length < min){
         alert("you entered to few characters");
         return false;
+    } else if(item.length > max){
+        alert("you entered to many characters")
+        return false;
+    } 
+
     else {
-        return true;
-    }
+            return true;
+        }
     }
 function numeric(field){
     let item = document.getElementById("field").value
+    //use REGEX for a item.length amount of characters
     alert("contains invalid characters");
     return false;
     else {
@@ -36,14 +47,18 @@ function numeric(field){
 }
 function required(field){
     let item = document.getElementById("field").value
-    if form is of class required{
-    alert('class that was required' +" is a field that you must fill out");
+    if(item != ''){
+    alert(item +" is a field that you must fill out")
     return false;}
     else {
         return true;
 }
 function not_required(field){
     let item = document.getElementById("field").value
-    if(field == "")
+    if(field == ""){
     return true
+    } else {
+    return false
+        //no idea how to handle this one, posible use an or statement?
+    }
 }
