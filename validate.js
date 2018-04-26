03b334b099a5056ef45d782fbd2c9b71a72a887a
codeman <clong@catalyte.io>
2018-04-26 10:15:36 -0700

    added return true to functions if they pass and started error messages

--- validate.js
+++ validate.js
@@ -6,13 +6,33 @@ function validateForm() {
         return false;
     }
 }
-def required_size(){
 
+//each field will be passed into various functions, and if it fails, it will return false with a descriptive error message
+
+function required_size(field, min, max){
+if(field.length > statedsize)
+    alert("you entered to few characters");
+    return false;
+else {
+    return true;
 }
-def numeric(){
-
 }
-def required(){
+function numeric(){
 
+    alert("Name must be filled out");
+    return false;
+    else {
+        return true;
 }
+function required(){
 
+    if form is of class required{
+    alert('class that was required' +" is a field that you must fill out");
+    return false;}
+    else {
+        return true;
+}
+function not_required(field){
+    if(field == "")
+    return true
+}
