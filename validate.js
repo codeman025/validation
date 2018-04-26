34f8234eaee4f46d2025190ccd4ec44332f5dc96
codeman <clong@catalyte.io>
2018-04-26 10:43:06 -0700

    added function form1 and form2 to separate the validation of the 2 forms

--- validate.js
+++ validate.js
@@ -8,19 +8,28 @@ function validateForm() {
 }
 
 //each field will be passed into various functions, and if it fails, it will return false with a descriptive error message
+// going to need to break up the forms into form 1 and for 2
+// probably just make 2 separate functions to deal with that.
+
+function form1(){
+    // validates only form 1's things, separate from form 2
+}
+function form2(){
+    //validates only the second 5 items
+}
 
 function required_size(field, min, max){
     let item = document.getElementById("field").value // sets item to the value of whatever field i am working with;
-if(item.length < min)
-    alert("you entered to few characters");
-    return false;
-else {
-    return true;
-}
-}
+    if(item.length < min)
+        alert("you entered to few characters");
+        return false;
+    else {
+        return true;
+    }
+    }
 function numeric(field){
     let item = document.getElementById("field").value
-    alert("Name must be filled out");
+    alert("contains invalid characters");
     return false;
     else {
         return true;
