568f053cc52c8b628c60c629c39d6ef0a6f67721
codeman <clong@catalyte.io>
2018-04-26 09:30:43 -0700

    added 3 functions from which to work from

---
+++ validate.js
@@ -0,0 +1,18 @@
+//if a name is equal to "" an alert pops up
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+def required_size(){
+
+}
+def numeric(){
+
+}
+def required(){
+
+}
+
