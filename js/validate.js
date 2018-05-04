2f22e8790d4f06ecd471233856a41af9aa4d7c79
codeman <clong@catalyte.io>
2018-05-04 10:34:37 -0700

    added numeric, used NaN as the qualifier

--- js/validate.js
+++ js/validate.js
@@ -8,18 +8,40 @@ window.onload = () => {//loads on start, lambda function
             event.preventDefault();//prevents sumbit
         });
     }
-}
+} 
 
-/* need to update everything to getByCLassName and then add it to an array of errors
-function numeric (field,index){
-    let item = document.getElementById(field).value;
-    if(item == isNaN(field)){
-        document.getElementsByClassName("errors")[index].textContent="This needs to be a number";
-        return false;
-    }else {
-        return true;
+function required_size(btn){
+    let size = document.getElementByClassName("required_size"); 
+    a = 0;
+    for(let i = 0; i < size.length; i++){
+        if (size[i].hasAttribute("maxlength")) {
+            if(size[i].value != ""){
+                let length = size[i].getAttribute("maxlength");
+                if (size[i].value.length != parseInt(length)) {
+                    errors(btn.parentElement).textContent += size[1].name + 
+                    " this field needs "+ length + " characters ";
+                    a++;
+                }
+            }
+        }
+        return a; // a is a text off errors
     }
-}
+    
+
+function numeric (btn){
+    let num = document.getElementByClassName("numeric"); //gets an array called num, iterates through checking it to see if its numeric
+    a = 0;
+    for(let i = 0; i < num.length; i++){//iterates through the fields that are numeric
+        if (num[i].hasAttribute("numeric")) {
+            if(num[i].value == NaN){//if the value is not a number (nan return this error field)
+                    errors(btn.parentElement).textContent += size[i].name + 
+                    " this field needs to be numbers ";
+                    a++;
+                }
+            }
+        }
+        return a; // a is a text off errors
+    }
 function required(field,index){
     let item = document.getElementById(field).value
     if(item != ''){
