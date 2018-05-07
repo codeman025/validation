d9db9da99ae0dc249a0801d35b4bc9db58b013ce
codeman <clong@catalyte.io>
2018-05-07 14:55:19 -0700

    going to append errors in my req functions, as well as prevent default straight from each function

--- MASTERFILE/js/validate.js
+++ MASTERFILE/js/validate.js
@@ -9,9 +9,9 @@ window.onload = () => {
             event.preventDefault(); // stops the sumbit button from validating the page
         }
         });
-    }
+    }console.log(document.getElementsByClassName());
 };
-
+/*
 function errors (form) {
     //make an array of error fields so its scaleable
     let errorArr = document.getElementsByClassName("errors");//makes an array of errors
@@ -22,21 +22,27 @@ function errors (form) {
     //append to the current form here??
 
 };
-
-function validateform (form) {
-    let errorList = '';//is this even where I want to add my errors?
+*/
+/* function validateform (form) {//specific set of form inputs
     let specificForm = event.target.parentElement;
-    let form = document.getElementsByClassName();//will need to get form specific array of inputs
-    for(let i = 0; i < form.length; i++){//run all required functions, currently isn't form specific
-        
+    //
+    let inputs = form.getElementsByClassName();//will need to get form specific array of inputs
+    for(let i = 0; i < inputs.length; i++){//run all required functions, currently isn't form specific
+        if(inputs[i].classList("required")){
+            input[i].required;
+        }else if(inputs[i].classList("required_size")){
+            input[i].required_size;
+    }else if(inputs[i].classList("numeric")){
+        input[i].required;
+    }else{
+        //so far..only goes through one class condition :(
     }
-    
-
- 
-};
+    }    
+};*/
 function required (form) {
     let errorList = '';
-    let req = document.getElementsByClassName("required");
+    let req = document.getElementsByClassName("required");//with parent class the same as the submit button
+    if (req.parentElement() == form.parentElement){
     for(let i = 0; i < req.length; i++){
         if(req[i]==""){
             errors(form.parentElement).textField = req + " is a required field";
@@ -44,6 +50,7 @@ function required (form) {
         }
     }
     return errorList;
+}
 };
 
 function required_size (form) {
@@ -62,7 +69,7 @@ function numeric (form){
     let num = document.getElementsByClassName("numeric");
         if(num == NaN){
             event.preventDefault();
-            errors(form.parentElement).textField = num + " this field needs to be numbers"
+            errors[form.parentElement].textField = num + " this field needs to be numbers"
     }
     return errorList;
 };
